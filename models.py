df02f52dc116b071079605e4e87537b46e0d8b89
Kausikram Krishnasayee <kausikram@gmail.com>
2012-10-11 10:47:47 +0530

    alpha version of the completely working app

--- models.py
+++ models.py
@@ -12,6 +12,35 @@ class BaseModel(peewee.Model):
 class Team(BaseModel):
     team_name = peewee.TextField()
 
+    @classmethod
+    def all(cls):
+        return [t for t in cls.select().where(True)]
+
+    def get_average_spirit_scores(self):
+        mts = MatchTeam.get_played_matches(team=self)
+        count = len(mts)
+        total = {
+            "rules" : 0,
+            "fouls" : 0,
+            "fair" : 0,
+            "positive" : 0,
+            "our_spirit" : 0,
+        }
+        if count == 0:
+            total["total"] = 0
+            return total
+
+        for mt in mts:
+            for k in total:
+                total[k] += getattr(mt.spirit_score,k)
+
+        average = {"total":0}
+        for k in total:
+            average[k] = float(total[k])/count
+            average["total"]+=average[k]
+
+        return average
+
     def __unicode__(self):
         return "<Team :{}>".format(self.team_name)
 
@@ -25,6 +54,15 @@ class Match(BaseModel):
         mt1 = MatchTeam.create_match_team(m, t1)
         mt2 = MatchTeam.create_match_team(m, t2)
 
+    def get_matchteams(self):
+        return [mt for mt in self.matchteam_set]
+
+    def get_teams(self):
+        return [mt.team for mt in self.get_matchteams()]
+
+    def get_score_string(self):
+        mts = self.get_matchteams()
+        return "{} - {}".format(mts[0].score, mts[1].score)
 
 class SpiritScore(BaseModel):
     rules = peewee.IntegerField(default=0)
@@ -39,6 +77,26 @@ class MatchTeam(BaseModel):
     team = peewee.ForeignKeyField(Team)
     score = peewee.IntegerField(default=0)
     spirit_score = peewee.ForeignKeyField(SpiritScore)
+
+    def get_opponent_mt(self):
+        mts = self.match.get_matchteams()
+        for mt in mts:
+            if mt.id != self.id:
+                return mt
+
+    def get_opponent(self):
+        return self.get_opponent_mt().team
+
+    @classmethod
+    def get_played_matches(cls, team):
+        all_mts = cls.filter(team = team)
+        played_matches = []
+        for mt in all_mts:
+            if mt.score == 0 and mt.get_opponent_mt().score == 0:
+                pass
+            else:
+                played_matches.append(mt)
+        return played_matches
 
     @classmethod
     def create_match_team(cls, match, team):
--- ultiscore.py
+++ ultiscore.py
@@ -1,26 +1,87 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+from models import *
+
+
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    database.connect()
+
+
+@app.teardown_request
+def after_request(ex):
+    database.close()
+
 
 @app.route('/')
 def show_scores():
     """ list all matches here"""
-    return 'Hello World!'
+    matches = Match.select().where(True)
+    return render_template("list_matches.html",matches=matches)
 
-@app.route('/create_team/')
+@app.route('/spirit/')
+def show_scores():
+    """ list all matches here"""
+    matches = Match.select().where(True)
+    return render_template("list_matches.html",matches=matches)
+
+@app.route('/teams/')
+def list_teams():
+    """ list all matches here"""
+    teams = Team.all()
+    return render_template("list_teams.html",teams=teams)
+
+
+@app.route('/create_team/', methods=['POST', 'GET'])
 def create_team():
-    return 'Hello World!'
+    if request.method == "GET":
+        return render_template("new_team_form.html")
+    if request.method == "POST":
+        Team.create(team_name = request.form['team_name'])
+        return redirect("/teams/")
 
-@app.route('/create_match/')
+@app.route('/create_match/', methods=['POST', 'GET'])
 def create_match():
-    return 'Hello World!'
+    teams = Team.all()
+    if request.method == "GET":
+        return render_template("new_match_form.html",teams=teams)
+    if request.method == "POST":
+        if request.form['team_1_id'] == request.form['team_2_id']:
+            return redirect("/create_match/")
+        t1 = Team.get(Team.id == request.form['team_1_id'])
+        t2 = Team.get(Team.id == request.form['team_2_id'])
+        m = Match.create_match(t1,t2)
+        return redirect("/")
 
-@app.route('/list_spirit_scores/')
-def enter_score_for_match():
-    return 'Hello World!'
 
-@app.route('/add_spirit_score/<match_id>/<team_id>/')
-def enter_spirit_score_for_team(team_id, match_id):
-    return 'Hello World!'
+@app.route('/edit_score/<int:match_id>/', methods=['POST', 'GET'])
+def enter_score_for_match(match_id):
+    match = Match.get(Match.id==match_id)
+    mts = [mt for mt in MatchTeam.filter(match__id=match_id)]
+    if request.method == "GET":
+        return render_template("edit_match_score.html",mts=mts)
+    if request.method == "POST":
+        for mt in mts:
+            mt.score = request.form['{}_score'.format(mt.id)]
+            mt.save()
+        return redirect("/")
+
+@app.route('/add_spirit_score/<int:mt_id>/', methods=['POST', 'GET'])
+def enter_spirit_score_for_team(mt_id):
+    mt = MatchTeam.get(MatchTeam.id == mt_id)
+    if request.method == "GET":
+        return render_template("spirit_form.html",mt=mt)
+    if request.method == "POST":
+        mt.spirit_score.rules = request.form["rules"]
+        mt.spirit_score.fouls = request.form["fouls"]
+        mt.spirit_score.fair = request.form["fair"]
+        mt.spirit_score.positive = request.form["positive"]
+        mt.spirit_score.our_spirit = request.form["our_spirit"]
+        mt.spirit_score.save()
+        mt.save()
+        return redirect("/teams/")
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
